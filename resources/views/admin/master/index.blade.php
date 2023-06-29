90a14400a44ecb2be80b6ea6018baf02bc74c1b7
Md Rezaul Karim Shanto <63625974+mdrkshanto@users.noreply.github.com>
2023-06-29 18:45:40 +0600

    Update index.blade.php

--- resources/views/admin/master/index.blade.php
+++ resources/views/admin/master/index.blade.php
@@ -348,9 +348,9 @@
 <!-- Switcher-->
 
 <!-- GLOBAL-LOADER -->
-{{--<div id="global-loader">--}}
-{{--    <img src="{{asset('/')}}admin/assets/images/loader.svg" class="loader-img" alt="Loader">--}}
-{{--</div>--}}
+<div id="global-loader">
+    <img src="{{asset('/')}}admin/assets/images/loader.svg" class="loader-img" alt="Loader">
+</div>
 <!-- /GLOBAL-LOADER -->
 
 <!-- PAGE -->
